fd0791f78d5a1bf0aad6619f226a9c0a284c7d98
Manuel3115 <manuel-etudes@outlook.com>
2023-09-11 20:04:39 -0400

    Small code quality improvements to make app scaling easier

--- src/Components/Employees.jsx
+++ src/Components/Employees.jsx
@@ -1,119 +1,28 @@
 import { useState } from "react";
+import { employeeData } from "../data/employees";
 
 const Employees = () => {
+  const locationOptions = ["ANY", "CANADA", "US", "ITALY"];
+
   const [checker, setChecker] = useState(0);
-  const [location, setLocation] = useState("CANADAUSAITALY");
-  const UpdateThing = () => { setChecker(1); }
-  const DeUpdateThing = () => { setChecker(0); }
-  const updateLocationToNone = () => { setLocation("CANADAUSAITALY") }
-  const updateLocationToCanada = () => { setLocation("CANADA") }
-  const updateLocationToUS = () => { setLocation("US") }
-  const updateLocationToItaly = () => { setLocation("ITALY") }
+  const [location, setLocation] = useState("ANY");
+  const [employees, setEmployees] = useState(employeeData);
 
-
-  const [employees, setEmployees] = useState([
-    {
-      id: 1000,
-      fullName: "Irelia Joestar",
-      designation: "Java dev",
-      pic: "/src/images/employee1.png",
-      location: "US"
-    },
-    {
-      id: 2000,
-      fullName: "Johnny Joestar",
-      designation: "Python dev",
-      pic: "/src/images/employee1.png",
-      location: "CANADA"
-
-    },
-    {
-      id: 3000,
-      fullName: "Jeffrey Jones",
-      designation: "Python dev",
-      pic: "/src/images/employee1.png",
-      location: "US"
-
-    },
-    {
-      id: 4000,
-      fullName: "Bob Jones",
-      designation: "Java dev",
-      pic: "/src/images/employee1.png",
-      location: "ITALY"
-
-    },
-    {
-      id: 5000,
-      fullName: "Alex Jones",
-      designation: "JS dev",
-      pic: "/src/images/employee1.png",
-      location: "ITALY"
-
-    },
-    {
-      id: 6000,
-      fullName: "John Doe",
-      designation: "Java dev",
-      pic: "/src/images/employee1.png",
-      location: "ITALY"
-
-    },
-    {
-      id: 4000,
-      fullName: "Bob Joestar",
-      designation: "Java dev",
-      pic: "/src/images/employee1.png",
-      location: "CANADA"
-
-    },
-    {
-      id: 4000,
-      fullName: "Denny Jones",
-      designation: "Java dev",
-      pic: "/src/images/employee1.png",
-      location: "CANADA"
-
-    },
-    {
-      id: 4000,
-      fullName: "Anthony Jones",
-      designation: "Java dev",
-      pic: "/src/images/employee1.png",
-      location: "US"
-
-    },
-    {
-      id: 4000,
-      fullName: "Amanda Joestar",
-      designation: "Java dev",
-      pic: "/src/images/employee1.png",
-      location: "CANADA"
-
-    },
-  ]);
   const Submenu = () => {
     return (
       <div style={{ position: "absolute", zIndex: 200 }}>
-        <button onMouseEnter={UpdateThing} onMouseLeave={DeUpdateThing} onClick={updateLocationToNone} style={{ width: 170 }}> NONE </button> <br />
-          <button onMouseEnter={UpdateThing} onMouseLeave={DeUpdateThing} onClick={updateLocationToCanada} style={{ width: 170 }}> CANADA </button> <br />
-          <button onMouseEnter={UpdateThing} onMouseLeave={DeUpdateThing} onClick={updateLocationToUS} style={{ width: 170 }}> US </button> <br />
-          <button onMouseEnter={UpdateThing} onMouseLeave={DeUpdateThing} onClick={updateLocationToItaly} style={{ width: 170 }}> ITALY </button> <br />
+        {locationOptions.map((location) => {
+          return <><button onMouseEnter={() => setChecker(1)} onMouseLeave={() => setChecker(0)} onClick={() => setLocation(location)} style={{ width: 170 }}> {location} </button><br /></>;
+        })}
       </div>
     )
   }
 
-
-
-
   const CardsEmployees = () => {
-
     return (
       <div className="row">
-        {employees.map((emp) => (
-          <>
-          {location.includes(emp.location) ?
-          <div className="col-4"> 
+        {employees.filter((emp) => { return (location === "ANY" || location === emp.location) }).map(emp =>
+          <div className="col-4">
             <div className="card m-2" style={{ cursor: "pointer" }}>
               <img className="rounded card-img-top" src={emp.pic} />
               <div className="card-body">
@@ -121,17 +30,16 @@ const Employees = () => {
                 <p className="card-text"> Designation: {emp.designation} <br />  id: {emp.id} <br /> {emp.location} </p>
                 <a href="#" className="btn btn-primary"> Contact </a>
               </div>
-            </div> </div>: <></>}
-          </>
-        ))
-        }
+            </div>
+          </div>
+        )}
       </div>
     )
   }
   return (
     <main className="container-fluid">
       <br />
-      <button className="rounded" onMouseEnter={UpdateThing} onMouseLeave={DeUpdateThing} style={{ backgroundColor: "white", color: "black", border: "solid" }}> Sort by Country</button>
+      <button className="rounded" onMouseEnter={() => setChecker(1)} onMouseLeave={() => setChecker(1)} style={{ backgroundColor: "white", color: "black", border: "solid" }}> Filter by Country</button>
       {checker ? <Submenu /> : <></>}
       <br />
       <br />
---
+++ src/data/employees.js
@@ -0,0 +1,81 @@
+export const employeeData = [
+    {
+        id: 1000,
+        fullName: "Irelia Joestar",
+        designation: "Java dev",
+        pic: "/src/images/employee1.png",
+        location: "US"
+    },
+    {
+        id: 2000,
+        fullName: "Johnny Joestar",
+        designation: "Python dev",
+        pic: "/src/images/employee1.png",
+        location: "CANADA"
+
+    },
+    {
+        id: 3000,
+        fullName: "Jeffrey Jones",
+        designation: "Python dev",
+        pic: "/src/images/employee1.png",
+        location: "US"
+
+    },
+    {
+        id: 4000,
+        fullName: "Bob Jones",
+        designation: "Java dev",
+        pic: "/src/images/employee1.png",
+        location: "ITALY"
+
+    },
+    {
+        id: 5000,
+        fullName: "Alex Jones",
+        designation: "JS dev",
+        pic: "/src/images/employee1.png",
+        location: "ITALY"
+
+    },
+    {
+        id: 6000,
+        fullName: "John Doe",
+        designation: "Java dev",
+        pic: "/src/images/employee1.png",
+        location: "ITALY"
+
+    },
+    {
+        id: 7000,
+        fullName: "Bob Joestar",
+        designation: "Java dev",
+        pic: "/src/images/employee1.png",
+        location: "CANADA"
+
+    },
+    {
+        id: 8000,
+        fullName: "Denny Jones",
+        designation: "Java dev",
+        pic: "/src/images/employee1.png",
+        location: "CANADA"
+
+    },
+    {
+        id: 9000,
+        fullName: "Anthony Jones",
+        designation: "Java dev",
+        pic: "/src/images/employee1.png",
+        location: "US"
+
+    },
+    {
+        id: 10000,
+        fullName: "Amanda Joestar",
+        designation: "Java dev",
+        pic: "/src/images/employee1.png",
+        location: "CANADA"
+
+    },
+]
